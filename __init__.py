b05393921dd51f4cedc887099e221af727bfc435
Sergii Serheiev <sergeevs@ukr.net>
2016-07-19 12:57:03 +0300

    Next commit2.

--- __init__.py
+++ __init__.py
@@ -13,7 +13,32 @@ for line in f.readlines():
 
 import sys
 print(sys.copyright, '\n\n', '\n sys.platform', sys.platform, '\n sys.path', sys.path, '\n sys.modules', sys.modules)
-'''
+
 
 stringus = "This {first:>10s} text locate {first} in {third: <20s} right place".format(first='10', second='20', third='30')
 print (stringus)
+
+l=[1, 22, 333, 4444, 5555]
+t=(10, 20, 30, 40, 50)
+
+print(t.__getitem__(3))
+#l1=str(l.__getitem__(2))
+#print(l1)
+print("{:>10}".format(str(l.__getitem__(2))))
+
+print(l[0:4])
+
+print (tuple(range(100, 20, -5)))
+
+s1 = "Never give UP!!!"
+print (s1.__len__())
+
+for i in s1:
+    print (i)
+'''
+
+s1 = "Never give UP!!!"
+
+b = '!!!!' in s1
+
+print(s1.replace("!!!", "QQQQQ"))
